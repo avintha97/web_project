4492d3f799008ba8d402adef533b07753a79f0bb
avintha97 <avintha.geo@gmail.com>
2023-04-18 14:02:55 +0530

    result page modified

--- user.php
+++ user.php
@@ -50,34 +50,32 @@ session_start();
             <div class="row row-cols-3 g-3">
                 <div class="col">
                     <div class="card">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                       
                         <div class="card-body">
                             <h5 class="card-title">Year I Semester I</h5>
                             <p class="card-text">
-                                avintha sandaruwan
+                            <a href="result.php"><button class="btn btn-success"> view my results</button></a> 
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top" alt="Palm Springs Road" />
+                        
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">Year I Semester II</h5>
                             <p class="card-text">
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                            <a href="result.php"><button class="btn btn-success"> view my results</button></a>
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers" />
+                        
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                                additional content.</p>
+                            <h5 class="card-title">Year II Semester I</h5>
+                            <p class="card-text"> <a href="result.php"><button class="btn btn-success"> view my results</button></a></p>
                         </div>
                     </div>
                 </div>
