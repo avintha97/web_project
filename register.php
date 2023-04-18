d159602935e342b4ab61206bb5889afe534beb93
avintha97 <avintha.geo@gmail.com>
2023-04-18 09:10:21 +0530

    test

--- register.php
+++ register.php
@@ -57,7 +57,7 @@ if(isset($_POST['submit'])){
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up form</p>
 
                 <form class="mx-1 mx-md-4" action="" method="post">
                  <?php
